ad23e33801db0a75d43e9d4328b4e546f8f19b98
Ciara Kang <ckof2022@gmail.com>
2024-11-13 22:28:30 -1000

    Issue #03: Set style for the project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,16 @@ import { Col, Container, Row } from 'react-bootstrap';
 
 /** The Home page. */
 const Home = () => (
-  <main>
+  <main style={{ fontFamily: 'Merriweather, serif', fontSize: '18px' }}>
     <Container id="landing-page" fluid className="d-flex justify-content-center align-items-center vh-150">
       <Row className="text-center">
         <Col xs={12}>
-          <div style={{ padding: '50px' }}>
+          <div
+            style={{
+              padding: '65px',
+              color: 'black',
+              textShadow: '2px 2px 4px #F0E68C, -2px -2px 4px #F0E68C' }}
+          >
             <h1>Welcome to your House Finding Journey</h1>
             <h5>
               <strong>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,8 @@
 import { useSession } from 'next-auth/react';
 import { usePathname } from 'next/navigation';
 import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
-import { BoxArrowRight, Lock, PersonFill, PersonPlusFill, HouseDoorFill, Search } from 'react-bootstrap-icons';
+import { BoxArrowRight, Lock, PersonFill, PersonPlusFill, HouseFill, Search } from 'react-bootstrap-icons';
+import Image from 'next/image';
 
 const NavBar: React.FC = () => {
   const { data: session } = useSession();
@@ -14,23 +15,38 @@ const NavBar: React.FC = () => {
   const role = userWithRole?.randomKey;
   const pathName = usePathname();
   return (
-    <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand href="/" style={{ fontFamily: 'Arial, sans-serif' }}>
-          <HouseDoorFill style={{ marginRight: '8px' }} />
-          House Finders
-        </Navbar.Brand>
+    <Navbar
+      bg="light"
+      expand="lg"
+      style={{ fontFamily: 'Merriweather, serif', display: 'flex', alignItems: 'center', fontSize: '22px' }}
+    >
+      <Container style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+      <Navbar.Brand
+        className="me-auto justify-content-left"
+        href="/"
+        style={{ fontFamily: 'Georgia, serif', display: 'flex', alignItems: 'center', fontSize: '28px' }}
+      >
+        <Image
+          src="/HF_logo.png"
+          alt="Logo"
+          width={80}
+          height={68}
+          style={{ marginRight: '8px' }}
+        />
+        House Finders
+      </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="me-auto justify-content-start">
+        <Nav className="ms-auto" style={{ marginRight: '28px' }}>
           <Nav.Link id="for-rent-nav" href="/for-rent" key="for-rent" active={pathName === '/for-rent'}>
+          <HouseFill style={{ marginRight: '6px', marginBottom: '5px' }} />
             For Rent
           </Nav.Link>
         </Nav>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Nav className="me-auto justify-content-end">
+        <Nav className="justify-content-center" style={{ marginRight: '30px' }}>
           <Nav.Link id="search-nav" href="/search" key="search" active={pathName === '/search'}>
-            <Search style={{ marginRight: '4px' }} />
+            <Search style={{ marginRight: '5px' }} />
             Search
           </Nav.Link>
         </Nav>
