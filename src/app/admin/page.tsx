a2cfaff130ed3c9252466d65e2bbdfa73a7e0529
Trevell Pruitt <trevell@hawaii.edu>
2024-11-20 20:34:40 -1000

    #issue 6 finished admin page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,65 +1,31 @@
 import { getServerSession } from 'next-auth';
-import { Col, Container, Row, Table } from 'react-bootstrap';
-import StuffItemAdmin from '@/components/StuffItemAdmin';
+import { Col, Container, Row } from 'react-bootstrap';
 import { prisma } from '@/lib/prisma';
 import { adminProtectedPage } from '@/lib/page-protection';
+import { User } from '@prisma/client';
 import authOptions from '@/lib/authOptions';
+import ContactCardAdmin from '@/components/ContactCardAdmin';
 
 const AdminPage = async () => {
+  const users: User[] = await prisma.user.findMany();
   const session = await getServerSession(authOptions);
   adminProtectedPage(
     session as {
-      user: { email: string; id: string; randomKey: string };
+      user: { email: string; id: string; randomKey: string; };
     } | null,
   );
-  const stuff = await prisma.stuff.findMany({});
-  const users = await prisma.user.findMany({});
 
   return (
     <main>
       <Container id="list" fluid className="py-3">
         <Row>
           <Col>
-            <h1>List Stuff Admin</h1>
-            <Table striped bordered hover>
-              <thead>
-                <tr>
-                  <th>Name</th>
-                  <th>Quantity</th>
-                  <th>Condition</th>
-                  <th>Owner</th>
-                  <th>Actions</th>
-                </tr>
-              </thead>
-              <tbody>
-                {stuff.map((item) => (
-                  <StuffItemAdmin key={item.id} {...item} />
-                ))}
-              </tbody>
-            </Table>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <h1>List Users Admin</h1>
-            <Table striped bordered hover>
-              <thead>
-                <tr>
-                  <th>Email</th>
-                  <th>Role</th>
-                  <th>Subrole</th>
-                </tr>
-              </thead>
-              <tbody>
-                {users.map((user) => (
-                  <tr key={user.id}>
-                    <td>{user.email}</td>
-                    <td>{user.role}</td>
-                    <td>{user.subrole}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </Table>
+            <h1 className="text-center">List Contact Admin</h1>
+            {users.map((user: User) => (
+              <Col key={user.id + user.role}>
+                <ContactCardAdmin user={user} />
+              </Col>
+            ))}
           </Col>
         </Row>
       </Container>
--- src/lib/validationSchemas.ts
+++ src/lib/validationSchemas.ts
@@ -14,3 +14,10 @@ export const EditStuffSchema = Yup.object({
   condition: Yup.string().oneOf(['excellent', 'good', 'fair', 'poor']).required(),
   owner: Yup.string().required(),
 });
+export interface User {
+  id: number;
+  email: string;
+  password: string;
+  role: string;
+  subrole: string;
+}
