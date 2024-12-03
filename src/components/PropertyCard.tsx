4112cbe5e1cae9e7ac681b11568b82bfa9686ab9
Michael Lee <mlee31@hawaii.edu>
2024-12-03 02:46:07 -1000

    Made list properties look nicer with cards

--- src/components/PropertyCard.tsx
+++ src/components/PropertyCard.tsx
@@ -2,29 +2,39 @@
 
 import { Property } from '@prisma/client';
 import Link from 'next/link';
-import { Card } from 'react-bootstrap';
+import { Card, ListGroup } from 'react-bootstrap';
 
 /* Renders a single Property. See list/page.tsx. */
 const PropertyCard = ({ property }: { property: Property }) => (
-  <Card>
+  <Card style={{ width: '20rem' }}>
+    <Card.Img src="./background.jpg" variant="top" height={180} />
     <Card.Body>
-      <Card.Title>
-        {property.address}
-      </Card.Title>
+      <Card.Title>{property.address}</Card.Title>
       <Card.Subtitle>
+        $&nbsp;
         {property.price}
       </Card.Subtitle>
       <Card.Text>
-        {property.bedrooms}
-        bedrooms,
-        {property.bathrooms}
-        bathrooms,
-        {property.sqft}
-        sqft
-      </Card.Text>
-      <Card.Footer>
+        Condition: &nbsp;
         {property.condition}
         <br />
+      </Card.Text>
+      <ListGroup className="list-group-flush">
+        <ListGroup.Item>
+          {property.bedrooms}
+          &nbsp; bedrooms
+        </ListGroup.Item>
+        <ListGroup.Item>
+          {property.bathrooms}
+          &nbsp; bathrooms
+        </ListGroup.Item>
+        <ListGroup.Item>
+          {property.sqft}
+          &nbsp; sqft
+        </ListGroup.Item>
+      </ListGroup>
+      <Card.Footer>
+        Landlord: &nbsp;
         {property.landlord}
       </Card.Footer>
       <Link href={`/edit/${property.id}`}>Edit</Link>
