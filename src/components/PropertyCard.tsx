a3936263840b0926944e13127e8a7f68789b37bf
domalian <121567094+domalian@users.noreply.github.com>
2024-12-05 12:33:05 -1000

    Issue 44: Set permission and style edit button

--- src/components/PropertyCard.tsx
+++ src/components/PropertyCard.tsx
@@ -1,49 +1,67 @@
 'use client';
 
 import { Property } from '@prisma/client';
+import { useSession } from 'next-auth/react';
 import Link from 'next/link';
 import { Card, ListGroup } from 'react-bootstrap';
+import { Pencil } from 'react-bootstrap-icons';
 import placeholder from '../../public/placeholder.png';
 
 /* Renders a single Property. See list/page.tsx. */
-const PropertyCard = ({ property }: { property: Property }) => (
-  <Card border="info" style={{ width: '20rem' }}>
-    <Card.Img src={placeholder.src} variant="top" height={180} />
-    <Card.Body>
-      <Card.Title>{property.address}</Card.Title>
-      <Card.Subtitle>
-        $&nbsp;
-        {property.price}
-      </Card.Subtitle>
-      <Card.Subtitle>
-        {property.id}
-      </Card.Subtitle>
-      <Card.Text>
-        Condition: &nbsp;
-        {property.condition}
-        <br />
-      </Card.Text>
-      <ListGroup className="list-group-flush">
-        <ListGroup.Item>
-          {property.bedrooms}
+const PropertyCard = ({ property }: { property: Property }) => {
+  const { data: session } = useSession();
+  const isOwner = session?.user?.email === property.landlord;
+
+  return (
+    <Card border="info" style={{ width: '20rem' }}>
+      <Card.Img src={placeholder.src} variant="top" height={180} />
+      <Card.Body>
+        <Card.Title>{property.address}</Card.Title>
+        <Card.Subtitle>
+          $&nbsp;
+          {property.price}
+        </Card.Subtitle>
+        <Card.Subtitle>
+          {property.id}
+        </Card.Subtitle>
+        <Card.Text>
+          Condition: &nbsp;
+          {property.condition}
+          <br />
+        </Card.Text>
+        <ListGroup className="list-group-flush">
+          <ListGroup.Item>
+            {property.bedrooms}
           &nbsp; bedrooms
-        </ListGroup.Item>
-        <ListGroup.Item>
-          {property.bathrooms}
+          </ListGroup.Item>
+          <ListGroup.Item>
+            {property.bathrooms}
           &nbsp; bathrooms
-        </ListGroup.Item>
-        <ListGroup.Item>
-          {property.sqft}
+          </ListGroup.Item>
+          <ListGroup.Item>
+            {property.sqft}
           &nbsp; sqft
-        </ListGroup.Item>
-      </ListGroup>
-      <Card.Footer>
-        Landlord: &nbsp;
-        {property.landlord}
-      </Card.Footer>
-      <Link href={`/edit/${property.id}`}>Edit</Link>
-    </Card.Body>
-  </Card>
-);
+          </ListGroup.Item>
+        </ListGroup>
+        <Card.Footer>
+          Landlord: &nbsp;
+          {property.landlord}
+        </Card.Footer>
+        {isOwner && (
+        <Link href={`/edit/${property.id}`} className="d-flex justify-content-end">
+          <button
+            type="button"
+            className="btn btn-primary btn-sm mt-2"
+            style={{ padding: '0.25rem 0.5rem', fontSize: '0.75rem', backgroundColor: 'green' }}
+          >
+            <Pencil color="white" />
+            <div>Edit</div>
+          </button>
+        </Link>
+        )}
+      </Card.Body>
+    </Card>
+  );
+};
 
 export default PropertyCard;
